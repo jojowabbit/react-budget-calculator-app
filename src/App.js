269635a73e21c35c8849bf3b89a1d9d56168512c
jojowabbit <jojowabbit@gmail.com>
2020-08-29 13:34:58 +0800

    added mockdata

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
 import React from "react";
+import { v4 as uuidv4 } from "uuid";
 
 import Alert from "./components/Alert";
 import ExpenseForm from "./components/ExpenseForm";
 import ExpenseList from "./components/ExpenseList";
 import "./App.css";
+
+// mock data
+const initialExpenses = [
+  {
+    id: uuidv4(),
+    charge: "rent",
+    amount: 1200,
+  },
+  {
+    id: uuidv4(),
+    charge: "food",
+    amount: 900,
+  },
+  {
+    id: uuidv4(),
+    charge: "transport",
+    amount: 300,
+  },
+];
 
 function App() {
   return (
