d00f78a4676189e1ece3666280a8f8735060b721
jojowabbit <jojowabbit@gmail.com>
2020-08-29 13:29:25 +0800

    initial & folder structure setup

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ExpenseItem = () => {
+  return <h1>ExpenseItem</h1>;
+};
+
+export default ExpenseItem;
---
+++ src/components/ExpenseList.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import Item from "./ExpenseItem";
+
+const ExpenseList = () => {
+  return <h1>ExpenseList</h1>;
+};
+
+export default ExpenseList;
