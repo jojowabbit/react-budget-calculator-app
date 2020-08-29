f48fb0f8628e38bf8b38448c0b2d1db97d726f4b
jojowabbit <jojowabbit@gmail.com>
2020-08-29 13:47:54 +0800

    App components setup & useState

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { v4 as uuidv4 } from "uuid";
 
 import Alert from "./components/Alert";
@@ -26,11 +26,21 @@ const initialExpenses = [
 ];
 
 function App() {
+  const [expenses, setExpenses] = useState(initialExpenses);
   return (
     <>
       <Alert />
-      <ExpenseForm />
-      <ExpenseList />
+      <h1>budget calculator</h1>
+      <main className="App">
+        <ExpenseForm />
+        <ExpenseList expenses={expenses} />
+      </main>
+      <h1>
+        total spending: RM
+        {expenses.reduce((acc, cur) => {
+          return (acc += cur.amount);
+        }, 0)}
+      </h1>
     </>
   );
 }
